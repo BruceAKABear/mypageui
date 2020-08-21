028295568092615ad334926ccf80f0a772e4d10f
dengyi <dengyi@dengyi.pro>
2020-08-21 14:07:40 +0800

    modify blog frame

--- src/views/front/Blog.vue
+++ src/views/front/Blog.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>hellpo</div>
+  <el-container>
+    <el-aside width="300px">
+      <el-card class="card-box">
+
+      </el-card>
+
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
 </template>
 
 <script>
@@ -11,6 +19,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .el-aside {
+    display: flex;
+    justify-content: center;
+    .card-box {
+      margin-top: 30px;
+      width: 250px;
+     height: 90%;
+    }
+  }
 
 </style>
