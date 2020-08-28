b427a1940fad8778f2c1dcf8984bdfd8a55a34a3
dengyi <dengyi@dengyi.pro>
2020-08-28 20:54:26 +0800

    modify blog frame

--- src/views/front/Blog.vue
+++ src/views/front/Blog.vue
@@ -1,33 +1,41 @@
 <template>
   <el-container>
+    <!--侧边-->
     <el-aside width="350px">
-      <el-card class="card-box">
-        <div class="content-box"></div>
-        <div class="pagination-box">
-          <el-pagination
-            background
-            layout="prev, pager, next"
-            :total="50">
-          </el-pagination>
+      <el-card shadow="hover" v-for="article in articleList " :key="article.id" :body-style="{ padding: '8px' }">
+        <div class="article-item-header">
+          <span>{{ article.title }}</span>
+        </div>
+        <div class="article-item-detail">
+          <span>时间:{{ article.updateTime }}</span>
+          <span>{{ article.author }}</span>
         </div>
       </el-card>
+      <!--分页-->
+      <div class="pagination-box">
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="50">
+        </el-pagination>
+      </div>
     </el-aside>
     <el-main>
       <!--文章导航-->
       <el-breadcrumb separator="/">
         <el-breadcrumb-item>博客</el-breadcrumb-item>
-        <el-breadcrumb-item>{{article.title}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ article.title }}</el-breadcrumb-item>
       </el-breadcrumb>
 
       <div class="article-box">
         <div class="article-title-box">
           <span>
-            {{article.title}}
+            {{ article.title }}
           </span>
           <div class="title-info-box">
-            <span>{{article.author}}</span>
-            <span>{{article.updateTime}}</span>
-            <i class="el-icon-star-on" style="color: red">{{article.favorite}}</i>
+            <span>{{ article.author }}</span>
+            <span>{{ article.updateTime }}</span>
+            <i class="el-icon-star-on" style="color: red">{{ article.favorite }}</i>
           </div>
         </div>
       </div>
@@ -46,51 +54,87 @@ export default {
         author: '邓艺',
         updateTime: '2020-10-10 10:10',
         favorite: 1000
-      }
+      },
+      articleList: [
+        {
+          title: 'Spring中bean的生命周期是什么',
+          author: '大熊',
+          updateTime: '2020-10-10 10:10',
+          favorite: 1000
+        }, {
+          title: 'Spring中bean的生命周期是什么',
+          author: '邓艺',
+          updateTime: '2020-10-10 10:10',
+          favorite: 1000
+        }, {
+          title: 'Spring中bean的生命周期是什么',
+          author: '邓艺',
+          updateTime: '2020-10-10 10:10',
+          favorite: 1000
+        }, {
+          title: 'Spring中bean的生命周期是什么',
+          author: '邓艺',
+          updateTime: '2020-10-10 10:10',
+          favorite: 1000
+        }, {
+          title: 'Spring中bean的生命周期是什么',
+          author: '邓艺',
+          updateTime: '2020-10-10 10:10',
+          favorite: 1000
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .el-aside {
+.el-aside {
+  padding: 10px;
+  border-right: solid 1px #EEEEEE;
+
+  .el-card {
+    margin-bottom: 10px;
+    font-size: 15px;
+
+    .article-item-detail {
+      margin-top: 8px;
+      display: flex;
+      justify-content: space-between;
+      font-size: 12px;
+      color: #9E9E9E;
+    }
+  }
+
+  .pagination-box {
     display: flex;
     justify-content: center;
+    position: absolute;
+    bottom: 30px;
+  }
+}
 
-    .card-box {
-      margin-top: 20px;
-      width: 300px;
-      height: 95%;
+.el-main {
+  .article-box {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
 
-      .pagination-box {
-        width: 100%;
+    .article-title-box {
+      font-size: 30px;
+      color: #999999;
+      text-align: center;
+
+      .title-info-box {
         display: flex;
-        justify-content: center;
+        justify-content: space-between;
+        font-size: 15px;
+        padding: 20px;
       }
     }
+
   }
-
-  .el-main {
-    .article-box {
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      margin-top: 20px;
-
-      .article-title-box {
-        font-size: 30px;
-        color: #999999;
-        text-align: center;
-
-        .title-info-box{
-          display: flex;
-          justify-content: space-between;
-          font-size: 15px;
-          padding: 20px;
-        }
-      }
-
-    }
-  }
+}
 
 </style>
