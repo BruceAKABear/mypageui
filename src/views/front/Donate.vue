2c562c143e214efb3ee8b4e64958a4b4d6df42d0
dengyi <dengyi@dengyi.pro>
2020-08-31 00:01:14 +0800

    modify donate

--- src/views/front/Donate.vue
+++ src/views/front/Donate.vue
@@ -6,11 +6,24 @@
       </div>
       <div class="desc-header-body">
         <span>
-       {{whyINeed}}
+       {{ whyINeed }}
       </span>
       </div>
-      <div class="opensource-project-info">
-        <span>开源项目进度</span>
+      <div class="opensource-desc-box">
+        <span>开源项目及进度</span>
+
+        <div class="project-item-box">
+          <div class="item-header-box">
+            <img src="../../assets/logo.png" height="60" width="60"/>
+            <span>无线键盘转换器</span>
+            <div>
+              <span>完成了吗</span>
+            </div>
+          </div>
+          <div class="item-progress-box">
+            <el-progress :text-inside="true" :stroke-width="26" :percentage="70"></el-progress>
+          </div>
+        </div>
       </div>
     </div>
     <div class="pay-box">
@@ -43,74 +56,94 @@ export default {
   data () {
     return {
       whyINeed: ' 由于开源项目需要消耗大量时间及精力进行开发，然而不管是开发完成还是开发过程中，我个人是没有任何收入的，为了能促进\n' +
-          '        我更好的进行开发，也为了能给大家带来更多更好玩、更实用的东西，在这里厚着脸皮想大家请求对我个人进行捐赠。捐赠不在\n' +
-          '        多少，如果你觉我的项目对您有所帮组自己经济又允许那么您可以多捐赠，如果只是鼓励我，那您也可以请我喝一瓶肥宅快乐水\n' +
-          '        对于捐赠后的您将获得DIY项目的优先购买权，以及相关资料。'
+        '        我更好的进行开发，也为了能给大家带来更多更好玩、更实用的东西，在这里厚着脸皮想大家请求对我个人进行捐赠。捐赠不在\n' +
+        '        多少，如果你觉我的项目对您有所帮组自己经济又允许那么您可以多捐赠，如果只是鼓励我，那您也可以请我喝一瓶肥宅快乐水\n' +
+        '        对于捐赠后的您将获得DIY项目的优先购买权，以及相关资料。'
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .content {
-    height: 100%;
-    width: 100%;
+.content {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  padding-top: 40px;
+
+  .desc-box {
+    width: 60%;
     display: flex;
-    padding-top: 40px;
+    flex-direction: column;
+    text-align: center;
 
-    .desc-box {
-      width: 60%;
-      display: flex;
-      flex-direction: column;
-      text-align: center;
-
-      .desc-header-box {
-        font-size: 36px;
-        font-weight: bold;
-        color: #757575;
-      }
-
-      .desc-header-body {
-        margin-top: 20px;
-        padding-left: 50px;
-        padding-right: 50px;
-        text-align: left;
-        font-size: 18px;
-        color: #999999;
-      }
+    .desc-header-box {
+      font-size: 36px;
+      font-weight: bold;
+      color: #757575;
     }
 
-    .pay-box {
-      width: 40%;
-      color: #9E9E9E;
+    .desc-header-body {
+      margin-top: 20px;
+      padding-left: 50px;
+      padding-right: 50px;
+      text-align: left;
+      font-size: 18px;
+      color: #999999;
+    }
+
+    .opensource-desc-box {
+      padding: 50px;
+      text-align: left;
       font-size: 20px;
       font-weight: bold;
-      display: flex;
-      justify-content: center;
+      color: #9E9E9E;
 
-      .pay-item-box {
-        width: 200px;
+      .project-item-box {
 
-        .item-header {
-          margin-bottom: 15px;
+        .item-header-box {
           display: flex;
           justify-content: space-between;
-          align-items: center;
-
-          .sub {
-            font-size: 10px;
-            margin-left: 20px;
-          }
+          font-size: 16px;
+          margin-bottom: 10px;
+          margin-top: 20px;
         }
 
       }
+    }
+  }
 
-      .margin {
-        margin-top: 60px;
+  .pay-box {
+    width: 40%;
+    color: #9E9E9E;
+    font-size: 20px;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+
+    .pay-item-box {
+      width: 200px;
+
+      .item-header {
+        margin-bottom: 15px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .sub {
+          font-size: 10px;
+          margin-left: 20px;
+        }
       }
 
     }
 
+    .margin {
+      margin-top: 60px;
+    }
+
   }
+
+}
 
 </style>
