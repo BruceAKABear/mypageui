1f227ec241570ca63100d74687f41c5229b27cc4
dengyi <dengyi@dengyi.pro>
2020-09-01 23:35:37 +0800

    modify blog

--- src/views/back/Write.vue
+++ src/views/back/Write.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
-    写作
+    <mavon-editor
+      v-model="content"
+      ref="md"
+      @change="change"
+      style="min-height: 600px"
+    />
   </div>
 </template>
 
 <script>
+// 导入组件 及 组件样式
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
 export default {
   name: 'Dashboard',
   data () {
     return {}
+  },
+  // 注册
+  components: {
+    mavonEditor
   }
 }
 </script>
