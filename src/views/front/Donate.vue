a72844ce401edda6bb38bb448bd9dd6078ca8611
dengyi <dengyi@dengyi.pro>
2020-09-04 22:30:44 +0800

    modify donate

--- src/views/front/Donate.vue
+++ src/views/front/Donate.vue
@@ -11,17 +11,17 @@
       </div>
       <div class="opensource-desc-box">
         <span>开源项目及进度</span>
-
-        <div class="project-item-box">
-          <div class="item-header-box">
-            <img src="../../assets/logo.png" height="60" width="60"/>
-            <span>无线键盘转换器</span>
-            <div>
-              <span>完成了吗</span>
+        <el-divider><i class="el-icon-menu" style="color: #9E9E9E"></i></el-divider>
+        <div class="project-item" v-for="project in openPorjects" :key="project.id">
+          <div class="project-item-header">
+            <el-avatar shape="square" size="medium"
+                       :src="project.pic"></el-avatar>
+            <div class="project-item-desc">
+              <span>{{ project.projectName }}</span>
             </div>
           </div>
-          <div class="item-progress-box">
-            <el-progress :text-inside="true" :stroke-width="26" :percentage="70"></el-progress>
+          <div class="project-item-progress">
+            <el-progress :text-inside="true" :stroke-width="26" :percentage="project.progress"></el-progress>
           </div>
         </div>
       </div>
@@ -58,7 +58,33 @@ export default {
       whyINeed: ' 由于开源项目需要消耗大量时间及精力进行开发，然而不管是开发完成还是开发过程中，我个人是没有任何收入的，为了能促进\n' +
         '        我更好的进行开发，也为了能给大家带来更多更好玩、更实用的东西，在这里厚着脸皮想大家请求对我个人进行捐赠。捐赠不在\n' +
         '        多少，如果你觉我的项目对您有所帮组自己经济又允许那么您可以多捐赠，如果只是鼓励我，那您也可以请我喝一瓶肥宅快乐水\n' +
-        '        对于捐赠后的您将获得DIY项目的优先购买权，以及相关资料。'
+        '        对于捐赠后的您将获得DIY项目的优先购买权，以及相关资料。',
+      openPorjects: [
+        {
+          id: '1',
+          pic: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+          projectName: '无线键盘转换器',
+          progress: 30
+        },
+        {
+          id: '2',
+          pic: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+          projectName: '无线麦克风',
+          progress: 60
+        },
+        {
+          id: '3',
+          pic: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+          projectName: '只能开关',
+          progress: 100
+        },
+        {
+          id: '4',
+          pic: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+          projectName: '不知道',
+          progress: 20
+        }
+      ]
     }
   }
 }
@@ -99,16 +125,20 @@ export default {
       font-weight: bold;
       color: #9E9E9E;
 
-      .project-item-box {
+      .project-item {
+        margin-top: 20px;
 
-        .item-header-box {
+        .project-item-header {
           display: flex;
-          justify-content: space-between;
-          font-size: 16px;
           margin-bottom: 10px;
-          margin-top: 20px;
-        }
+          align-items: center;
 
+          .project-item-desc {
+            margin-left: 15px;
+            font-size: 16px;
+
+          }
+        }
       }
     }
   }
